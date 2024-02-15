e141865f02d6fd91e6717435e6c0b2c5f326113c
emohawk13 <emohawk13@gmail.com>
2024-02-15 23:30:36 +0000

    inital commit

--- README.md
+++ README.md
@@ -1,27 +1,17 @@
-Assignment - Loops Challenges
+1st Challenge
+Previously, in the JS Data Structures Challenges we completed a program that alternately removed removes from the bacfk and front of an array each time the program ran. Use the same concept with classes while completing the following:
 
-Use loops and previous coding foundation concepts to solve the following challenges in JavaScript. Note that no outisde libraries may be used to complete these challenges:
 
-Write a program that counts to 10 by 2's
+Create a class that manages an array of items
+Create a function that will:
+splice the managed arrays of items
+return each item from alternating sides of the array until the array is empty
+Consult the following example of a successful alternating splice function on the created class as you create your own:
 
-Write a program that extracts values form an array at specific indices and returns the values in a new array.
-Here is an example of potential output: removeByIndex[‘a', 'b', 'c', 'd', 'f', 'f', 'e', 'f'], [1, 5]) => ['b', 'f']]
-
-Write a program that converts a number into an array of numbers. Ensure each element in the array is a number data type and NOT a string data type.
-Here is an example of potential output: numToArray(12345) => [1, 2, 3, 4, 5]
-
-Write a program to create a new matrix of arrays from two arrays of numbers and strings.
-Here is an example of potential output:
-arrayOne = [1, 2, 3]
-arrayTwo =  ["a", "b", "c", "d"]
- 
-// OUTPUT: [
-    [1, 'a'], ['a', 1], [1, 'b'],
-    ['b', 1], [1, 'c'], ['c', 1],
-    [1, 'd'], ['d', 1], [2, 'a'],
-    ['a', 2], [2, 'b'], ['b', 2],
-    [2, 'c'], ['c', 2], [2, 'd'],
-    ['d', 2], [3, 'a'], ['a', 3],
-    [3, 'b'], ['b', 3], [3, 'c'],
-    ['c', 3], [3, 'd'], ['d', 3]
-]
+[1, 2, 3, 4, 5]
+class.pop() => 1
+class.pop() => 5
+class.pop() => 2
+class.pop() => 4
+class.pop() => 3
+class.pop() => ''
--- scripts/common-scripts.js
+++ scripts/common-scripts.js
@@ -1,39 +1,21 @@
-// Write a program that counts to 10 by 2's
-console.log('Counting by 2\'s & 10\'s');
-console.log('');
-for (let i = 0; i <= 10; i += 2) {
-    console.log(i);
-}
-
-// Write a program that extracts values form an array at specific indices and returns the values in a new array.
-// Here is an example of potential output: removeByIndex[‘a', 'b', 'c', 'd', 'f', 'f', 'e', 'f'], [1, 5]) => ['b', 'f']]
-function removeByIndex(arr, indices) {
-    return indices.map(index => arr[index]);
-}
-console.log(removeByIndex(['a', 'b', 'c', 'd', 'f', 'f', 'e', 'f'], [3, 2]));
-
-
-// Write a program that converts a number into an array of numbers. Ensure each element in the array is a number data type and NOT a string data type.
-// Here is an example of potential output: numToArray(12345) => [1, 2, 3, 4, 5]
-function numToArray(num) {
-    return num.toString().split('').map(Number);
-}
-console.log(numToArray(123456789));
-
-
-// Write a program to create a new matrix of arrays from two arrays of numbers and strings.
-// Here is an example of potential output:
-// arrayOne = [1, 2, 3]
-// arrayTwo =  ["a", "b", "c", "d"]
-
-function createMatrix(arr1, arr2) {
-    const matrix = [];
-    for (let i = 0; i < arr1.length; i++) {
-        matrix.push([arr1[i], arr2[i]]);
+class ArrayManager {
+    constructor(array) {
+        this.array = array;
     }
-    return matrix;
+
+    pop() {
+        if (this.array.length === 0) {
+            return '';
+        }
+        const item = this.array.length % 2 === 0 ? this.array.shift() : this.array.pop();
+        return item;
+    }
 }
 
-const arrayOne = [1, 2, 3, 4];
-const arrayTwo = ['a', 'b', 'c', 'd'];
-console.log(createMatrix(arrayOne, arrayTwo));
+const arrayManager = new ArrayManager([1, 2, 3, 4, 5]);
+console.log(arrayManager.pop());
+console.log(arrayManager.pop());
+console.log(arrayManager.pop());
+console.log(arrayManager.pop());
+console.log(arrayManager.pop());
+console.log(arrayManager.pop());
