f432db8dd494df8637ad69c84beefa0b646dfae8
emohawk13 <dauhson13@gmail.com>
2024-08-08 11:44:54 -0600

    refactor commit

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ Create two objects, each with different key and value pairs from the other objec
 Here is an example of the output:
 // first object output
 { id: 1, name: “ryan”}
- 
+
 // second object output
 { address: “123 fake st”,
 Phone: “555-555-5555”}
- 
+
 // third object output
 { id: 1, name: “ryan”, address: “123 fake st”, phone: “555-555-5555”}
 
@@ -27,3 +27,9 @@ BONUS: Make the output represent the input characters (each character returns ca
 // The spread function is a great option to placing object literals in a a single object literal. For an additional challenge, use a loop to access each key dynamically and set their key and value in the new object.
 
 // The lines with multiple methods together and be broken down into variables that indicate what value is desired from each line. This will help any developer look at your code and understand each part of the lines.
+
+redo comments:
+
+1. The spread function is a great option to placing object literals in a a single object literal. For an additional challenge, use a loop to access each key dynamically and set their key and value in the new object. This did not affect your score.
+
+2. Your code properly works with several methods on one line. Though this is possible, because we work in a dev shop, we strive to make our code as explicit as possible. The lines with multiple methods together and be broken down into variables that indicate what value is desired from each line. This will help any developer look at your code and understand each part of the lines.
--- scripts/common-scripts.js
+++ scripts/common-scripts.js
@@ -1,35 +1,40 @@
-// 1. Merging Objects
 const obj1 = { id: 1, name: "ryan" };
 const obj2 = { address: "123 fake st", phone: "555-555-5555" };
-const mergedObj = { ...obj1, ...obj2 };
 
-// 2. Finding the Largest Number using if and ternary operator
+let mergedObj = { ...obj1, ...obj2 };
+
 function largestNum(a, b) {
-    return a > b ? a : b;
+  return a > b ? a : b;
 }
 
-// 3. Title Casing a String
 function titleCase(str) {
-    return str
-        .split(' ')
-        .map(word => {
-            const firstChar = word.charAt(0).toUpperCase();
-            const restChars = word.slice(1).toLowerCase();
-            return firstChar + restChars;
-        })
-        .join(' ');
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ");
 }
 
-// 4. Alphabetical Order of Characters
 function alphabeticalOrder(str) {
-    return str
-        .split('')
-        .sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }))
-        .join('');
+  return str
+    .split("")
+    .sort((a, b) => a.localeCompare(b))
+    .join("");
+}
+function preserveCaseAlphabeticalOrder(str) {
+  let sortedLowerCase = alphabeticalOrder(str.toLowerCase());
+  let mapping = str.split("").reduce((acc, char) => {
+    acc[char.toLowerCase()] = acc[char.toLowerCase()] || char;
+    return acc;
+  }, {});
+
+  return sortedLowerCase
+    .split("")
+    .map((char) => mapping[char])
+    .join("");
 }
 
-// Logs to fire functions
 console.log(mergedObj);
-console.log(largestNum(5, 43));
+console.log(largestNum(9, 12));
 console.log(titleCase("I am a sentence"));
 console.log(alphabeticalOrder("Hi there"));
+console.log(preserveCaseAlphabeticalOrder("Hi there"));
